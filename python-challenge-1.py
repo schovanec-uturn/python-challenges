37e7326c8a4d2278710d0db88425c0db83eab918
Sarah Chovanec <schovanec@uturndata.com>
2023-11-09 17:02:17 -0500

    Final Product

--- python-challenge-1.py
+++ python-challenge-1.py
@@ -20,14 +20,12 @@ newlist = [] # list of the input city amount
 linenum = [] # want this to be the list of the row # captured city is on out of the whole list
 
 i = 0
-while i <= len(newlist):
-    answer = input(str("what city? "))
-    for cityname in cities:
-        if cityname == answer:
-            newlist.append(cityname)
-            if
-            linenum.append(i)
-            i += 1
-    citycount = len(newlist)
-    print(citycount)
-    print(i)
+
+answer = input(str("what city? "))
+for cityname in cities:
+    i += 1
+    if cityname == answer:
+        # newlist.append(cityname)
+        linenum.append(i)
+# citycount = len(newlist)
+print(answer, "is on lines", linenum)
